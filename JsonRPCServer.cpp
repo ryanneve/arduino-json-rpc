464b2cb5f9f04a39e6bc6aaea247fb569ec72d4f
frmdstryr <frmdstryr@gmail.com>
2016-11-22 16:04:21 -0500

    Update JsonRPCServer.cpp

--- JsonRPCServer.cpp
+++ JsonRPCServer.cpp
@@ -112,7 +112,7 @@ void JsonRPCServer::processMessage(aJsonObject *msg) {
 				}
 				case JSON_RPC_RET_TYPE_OBJECT: {
 					JSON_PROC_OBJECT_STATIC_T callback = (JSON_PROC_OBJECT_STATIC_T) mapping->callback;
-					aJsonObject* ret = callback(this, params);
+					aJsonObject *ret = callback(this, params);
 					aJson.addItemToObject(response, "result", ret);
 					aJson.print(response, &_jsonStream);
 					aJson.deleteItem(ret);
