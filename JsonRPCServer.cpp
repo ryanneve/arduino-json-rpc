67412fe4581436b7f03580b641b3cfa9030f7027
frmdstryr <frmdstryr@gmail.com>
2016-11-21 20:51:20 -0500

    Update JsonRPCServer.cpp

--- JsonRPCServer.cpp
+++ JsonRPCServer.cpp
@@ -1,7 +1,7 @@
 /*
   JsonRPCServer.cpp - Simple JSON-RPC Server for Arduino
   Created by Meir Tseitlin, March 5, 2014. This code is based on https://code.google.com/p/ajson-rpc/
-  Modified by frmdstryr, 19 Nov 2016
+  Modified by frmdstryr, March, 2016 to add better json 2.0 support.
   Released under GPLv2 license.
 */
 #include "Arduino.h"
@@ -13,24 +13,21 @@ JsonRPCServer::JsonRPCServer(Stream* stream): _jsonStream(stream) {
 }
 
 
-void JsonRPCServer::begin(int capacity)
-{
-    mymap = (FuncMap *)malloc(sizeof(FuncMap));
-    mymap->capacity = capacity;
-    mymap->used = 0;
-    mymap->mappings = (Mapping*)malloc(capacity * sizeof(Mapping));
-    memset(mymap->mappings, 0, capacity * sizeof(Mapping));
+void JsonRPCServer::begin(int capacity) {
+    registry = (FuncMap *)malloc(sizeof(FuncMap));
+    registry->capacity = capacity;
+    registry->used = 0;
+    registry->mappings = (Mapping*)malloc(capacity * sizeof(Mapping));
+    memset(registry->mappings, 0, capacity * sizeof(Mapping));
 	
 	// Register all json procedures (pure virtual - implemented in derivatives with macros)
 	registerProcs();
 }
 
-void JsonRPCServer::registerMethod(String methodName, JSON_PROC_STATIC_T callback, JSON_RPC_RET_TYPE type)
-{
+void JsonRPCServer::registerMethod(String methodName, JSON_PROC_STATIC_T callback, JSON_RPC_RET_TYPE type) {
     // only write keyvalue pair if we allocated enough memory for it
-    if (mymap->used < mymap->capacity)
-    {
-	    Mapping* mapping = &(mymap->mappings[mymap->used++]);
+    if (registry->used < registry->capacity) {
+	    Mapping* mapping = &(registry->mappings[registry->used++]);
 	    mapping->name = methodName;
 	    mapping->callback = callback;
 		mapping->retType = type;
@@ -38,93 +35,97 @@ void JsonRPCServer::registerMethod(String methodName, JSON_PROC_STATIC_T callbac
 }
 
 
-void JsonRPCServer::processMessage(aJsonObject *msg)
-{
-    aJsonObject* method = aJson.getObjectItem(msg, "method");
+void JsonRPCServer::processMessage(aJsonObject *msg) {
+	// Create response
     aJsonObject *response = aJson.createObject();
-    if (!method)
-    {
+    aJson.addItemToObject(response,"jsonrpc",aJson.createItem("2.0"));
 
+    // Check required fields
+    aJsonObject *method = aJson.getObjectItem(msg, "method");
+    aJsonObject *id = aJson.getObjectItem(msg, "id");
+
+    if (!method || !id) {
+    	// Not a valid Json-RPC 2.0 message
     	aJsonObject *error = aJson.createObject();
-    	// not a valid Json-RPC message
     	aJson.addItemToObject(error, "code", aJson.createItem(-32600));
     	aJson.addItemToObject(error, "message", aJson.createItem("Invalid Request."));
     	aJson.addItemToObject(response,"error",error);
+
+    	if (!id) {
+    		aJson.addItemToObject(response,"id",aJson.createNull());
+    		aJson.addItemToObject(error, "data", aJson.createItem("Missing id."));
+    	} else {
+    		aJson.addItemToObject(response,"id",id);
+    		aJson.addItemToObject(error, "data", aJson.createItem("Missing method."));
+    	}
+
 		aJson.print(response, &_jsonStream);
-		aJson.deleteItem(error);
 		aJson.deleteItem(response);
         return;
     }
     
+    // Add the id
+    aJson.addItemToObject(response,"id",id);
+
+    // Get the params (if given)
     aJsonObject* params = aJson.getObjectItem(msg, "params");
 
-    
     String methodName = method->valuestring;
-    for (unsigned int i=0; i<mymap->used; i++)
-    {
-        Mapping* mapping = &(mymap->mappings[i]);
-        if (methodName.equals(mapping->name))
-        {
+    for (unsigned int i=0; i<registry->used; i++) {
+        Mapping* mapping = &(registry->mappings[i]);
+        if (methodName.equals(mapping->name)) {
 			switch (mapping->retType) {
-			case	JSON_RPC_RET_TYPE_NONE:
-				mapping->callback(this, params);
-				aJson.addItemToObject(response, "result", aJson.createNull());
-				break;
-			case	JSON_RPC_RET_TYPE_INT:
-				{
-					JSON_PROC_INT_STATIC_T callback = (JSON_PROC_INT_STATIC_T) mapping->callback;
-					
-					int ret = callback(this, params);
-					aJson.addItemToObject(response, "result", aJson.createItem((int)ret));
+				case JSON_RPC_RET_TYPE_NONE: {
+					mapping->callback(this, params);
+					aJson.addItemToObject(response, "result", aJson.createNull());
+					break;
 				}
-				break;
-			case	JSON_RPC_RET_TYPE_BOOL:
-				{
+				case JSON_RPC_RET_TYPE_INT: {
+					JSON_PROC_INT_STATIC_T callback = (JSON_PROC_INT_STATIC_T) mapping->callback;
+					int ret = callback(this, params);
+					aJson.addItemToObject(response, "result", aJson.createItem(ret));
+					break;
+				}
+				case JSON_RPC_RET_TYPE_BOOL: {
 					JSON_PROC_BOOL_STATIC_T callback = (JSON_PROC_BOOL_STATIC_T) mapping->callback;
-					
 					bool ret = callback(this, params);
 					aJson.addItemToObject(response, "result", aJson.createItem(ret));
+					break;
 				}
-				break;
-			case	JSON_RPC_RET_TYPE_FLOAT:
-				{
+				case JSON_RPC_RET_TYPE_FLOAT: {
 					JSON_PROC_FLOAT_STATIC_T callback = (JSON_PROC_FLOAT_STATIC_T) mapping->callback;
-
 					float ret = callback(this, params);
 					aJson.addItemToObject(response, "result", aJson.createItem(ret));
+					break;
 				}
-				break;
-			case	JSON_RPC_RET_TYPE_DOUBLE:
-				{
+				case JSON_RPC_RET_TYPE_DOUBLE: {
 					JSON_PROC_DOUBLE_STATIC_T callback = (JSON_PROC_DOUBLE_STATIC_T) mapping->callback;
-
 					double ret = callback(this, params);
 					aJson.addItemToObject(response, "result", aJson.createItem(ret));
+					break;
 				}
-				break;
-			case	JSON_RPC_RET_TYPE_STRING:
-				{
+				case JSON_RPC_RET_TYPE_STRING: {
 					JSON_PROC_STRING_STATIC_T callback = (JSON_PROC_STRING_STATIC_T) mapping->callback;
 					String ret = callback(this, params);
 					aJson.addItemToObject(response, "result", aJson.createItem(ret.c_str()));
-					
+					break;
 				}
-				break;
-//			case	JSON_RPC_RET_TYPE_OBJECT:
-//				{
-//					JSON_PROC_OBJECT_T callback = (JSON_PROC_OBJECT_T) mapping->callback;
-//					aJsonObject ret = callback(this, params);
-//					aJson.addItemToObject(response, "result", ret);
-//					aJson.print(response, &_jsonStream);
-//					aJson.deleteItem(ret);
-//					aJson.deleteItem(response);
-//					return;
-//
-//				}
-//				break;
+				case JSON_RPC_RET_TYPE_OBJECT: {
+					JSON_PROC_OBJECT_STATIC_T callback = (JSON_PROC_OBJECT_STATIC_T) mapping->callback;
+					aJsonObject ret = callback(this, params);
+					aJson.addItemToObject(response, "result", &ret);
+					aJson.print(response, &_jsonStream);
+					aJson.deleteItem(&ret);
+					aJson.deleteItem(method);
+					aJson.deleteItem(id);
+					aJson.deleteItem(response);
+					return;
+				}
 			}
 			
 			aJson.print(response, &_jsonStream);
+			aJson.deleteItem(method);
+			aJson.deleteItem(id);
 			aJson.deleteItem(response);
 			return;
 		}
@@ -137,6 +138,8 @@ void JsonRPCServer::processMessage(aJsonObject *msg)
 	aJson.addItemToObject(response,"error",error);
 	aJson.print(response, &_jsonStream);
 	aJson.deleteItem(error);
+	aJson.deleteItem(method);
+	aJson.deleteItem(id);
 	aJson.deleteItem(response);
 }
 
@@ -155,15 +158,17 @@ void JsonRPCServer::process() {
 		if (msg) {
 			processMessage(msg);
 			aJson.deleteItem(msg);
-			} else {
-				aJsonObject *response = aJson.createObject();
-				aJsonObject *error = aJson.createObject();
-				aJson.addItemToObject(error, "code", aJson.createItem(-32700));
-				aJson.addItemToObject(error, "message", aJson.createItem("Parse error."));
-				aJson.addItemToObject(response,"error",error);
-				aJson.print(response, &_jsonStream);
-				aJson.deleteItem(error);
-				aJson.deleteItem(response);
+		} else {
+			aJsonObject *response = aJson.createObject();
+			aJsonObject *error = aJson.createObject();
+			aJson.addItemToObject(response,"jsonrpc",aJson.createItem("2.0"));
+			aJson.addItemToObject(response,"id",aJson.createNull());
+			aJson.addItemToObject(error, "code", aJson.createItem(-32700));
+			aJson.addItemToObject(error, "message", aJson.createItem("Parse error."));
+			aJson.addItemToObject(response,"error",error);
+			aJson.print(response, &_jsonStream);
+			aJson.deleteItem(error);
+			aJson.deleteItem(response);
 		}
 	}
 	
